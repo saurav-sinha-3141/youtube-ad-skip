c33a5a1a6f2bae240a5371cecb8eae10067cdbc8
saurav-sinha-3141 <sauravsinha3141@gmail.com>
2024-12-23 22:06:09 +0530

    FIX skip button click

--- content.js
+++ content.js
@@ -2,10 +2,10 @@ function checkForAds() {
   console.log("YouTube Ad Skipper is running");
 
   const adContainer = document.querySelector(".ad-showing");
-  const skipButton = document.querySelector(".ytp-ad-skip-button");
+  const skipButton = document.querySelector(".ytp-skip-ad-button");
 
   if (adContainer) {
-    document.querySelector("video").playbackRate = 16;
+    document.querySelector("video").playbackRate = 4;
     console.log("Speeding up the ad");
   }
 
